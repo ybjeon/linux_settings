3dca73b69f2cf36dd7708a9b134d1c7732d41d54
spai-ybjeon <jyb9443@gmail.com>
2016-08-16 00:42:21 +0900

    distinguish GUI from CLI

--- install.sh
+++ install.sh
@@ -1,25 +1,38 @@
+if [ "$1" = "gui" ]; then
+	gui=true
+	echo "Installing for GUI"
+else
+	echo "Installing for CLI"
+fi
+
+# vim & plugins
 sudo apt-get install vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~
 
 sudo apt-get install tmux
 sudo apt-get install git
-sudo apt-get install guake
+sudo apt install htop
 cp -r .gconf ~
 sudo apt-get install python-pip
 sudo apt-get install python-virutalenv	
+if $gui; then
+sudo apt-get install guake
+fi
 # install and setting zsh
 sudo apt-get install zsh
 sudo apt-get install git-core
 
 # oh-my-zsh
+cp .zshrc ~
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 source ~/.zshrc
-cp .vimrc ~
-cp .zshrc ~
 chsh -s $(which zsh)
 
 #etc
 sudo apt-get install htop
+if $gui; then
 sudo apt-get install xclip
+fi
