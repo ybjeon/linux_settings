a9da17804be7f9cfcb56e4a33c89c37f8fae0696
ybjeon <jyb9443@gmail.com>
2018-04-26 23:55:21 +0900

    Add always yes options to "apt install" Change the order of "git" package correctly

--- install.sh
+++ install.sh
@@ -5,37 +5,37 @@ else
 	echo "Installing for CLI"
 fi
 
+# git
+sudo apt install -y git
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
 # vim & plugins
-sudo apt-get install vim
+sudo apt install -y vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~
 
 # tmux
-sudo apt-get install tmux
+sudo apt install -y tmux
 cp .tmux.conf ~
-
-# git
-sudo apt-get install git
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 sudo apt install htop
 cp -r .gconf ~
-sudo apt-get install python-pip
-sudo apt-get install python-virutalenv	
+sudo apt install -y python-pip
+sudo apt install -y python-virutalenv	
 if $gui; then
-sudo apt-get install guake
+sudo apt install -y guake
 fi
 
 if $gui; then
 # sublime text
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt install -y sublime-text
 fi
 
 # install and setting zsh
-sudo apt-get install zsh
-sudo apt-get install git-core
+sudo apt install -y zsh
+sudo apt install -y git-core
 
 # oh-my-zsh
 cp .zshrc ~
@@ -46,7 +46,7 @@ source ~/.zshrc
 chsh -s $(which zsh)
 
 #etc
-sudo apt-get install htop
+sudo apt install -y htop
 if $gui; then
-sudo apt-get install xclip
+sudo apt install -y xclip
 fi
