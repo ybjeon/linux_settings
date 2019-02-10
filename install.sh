4c5f8c4154f12d33adff759eab390d1b4397a237
ybjeon <jyb9443@gmail.com>
2019-02-10 22:04:16 +0900

    fix sublime text-3 install script

--- install.sh
+++ install.sh
@@ -30,9 +30,10 @@ sudo apt-get install software-properties-common
 
 if $gui; then
 # sublime text
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install -y sublime-text
+sudo apt-get install sublime-text
 fi
 
 # install and setting zsh
